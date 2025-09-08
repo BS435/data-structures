1357d6163b6d70e158feebb75ca00fe51cb2ad39
Brandon Suárez <143466135+BS435@users.noreply.github.com>
2025-09-08 15:57:58 -0600

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 10
+int top = -1;
+int stack[MAX];
+
+bool is_empty() {
+    if (top == -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool is_full() {
+    if (top == MAX - 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void push(int data) {
+    if (is_full()) {
+        cout << "Overflow - Stack is full" << endl;
+    } else {
+        top++;
+        stack[top] = data;
+    }
+}
+
+int pop() {
+    if (is_empty()) {
+        cout << "Underflow - Stack is empty" << endl;
+        return -1;
+    } else {
+        int data = stack[top];
+        top--;
+        return data;
+    }
+}
+
+int main() {
+    
+    push(10);
+    push(20);
+    push(30);
+
+    cout << "Popped element: " << pop() << endl;
+    cout << "Popped element: " << pop() << endl;
+    cout << "Popped element: " << pop() << endl;
+    cout << "Popped element: " << pop() << endl;
+
+    return 0;
+}
